2dba62de6f1711d0ab32ebffc9d190e567a936a8
Bruno Wu <wubruno@outlook.com>
2024-01-10 17:20:39 +0000

    new setup

---
+++ features/main/Main.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { useEffect } from "react";
+import styled from "styled-components";
+import { Fieldset, Panel } from "react95";
+
+import Contracts from "../../containers/Contracts";
+import OutputLog from "../output-log/OutputLog";
+import AddressInfo from "../address-info/AddressInfo";
+
+const Container = styled.div`
+  flex-grow: 1;
+  margin-left: 1rem;
+`;
+
+const ContentFrame = styled(Fieldset)`
+  height: calc(100% - 8px);
+`;
+
+const Content = styled.div`
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-rows: 1fr 1fr 28px;
+  grid-gap: 16px;
+`;
+
+const TopContainer = styled.div`
+  display: flex;
+`;
+
+const FooterPanel = styled(Panel)`
+  padding: 0.1rem 0.25rem;
+  width: 100%;
+  font-size: 14px;
+  white-space: nowrap;
+  overflow: hidden;
+  line-height: 20px;
+`;
+
+const Main = () => {
+  const { selectedContract: contract, overwriteContract } =
+    Contracts.useContainer();
+
+  useEffect(() => {
+    const contracts = localStorage.getItem("contracts");
+    if (!contracts) {
+      return;
+    }
+    const result = `{"data":${contracts}}`;
+    let jsonResult = JSON.parse(result).data;
+    overwriteContract(jsonResult);
+  }, []);
+
+  return (
+    <Container>
+      <ContentFrame label={contract && contract.name}>
+        <Content>
+          <TopContainer>
+            <AddressInfo />
+            <OutputLog />
+          </TopContainer>
+          <FooterPanel variant="well">
+            {`Path: ${contract?.path}` || "No path specified for this contract"}
+          </FooterPanel>
+        </Content>
+      </ContentFrame>
+    </Container>
+  );
+};
+
+export default Main;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,46 +1,8 @@
 import "../styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import type { AppProps } from "next/app";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import {
-  arbitrum,
-  goerli,
-  mainnet,
-  optimism,
-  polygon,
-  base,
-  zora,
-  arbitrumSepolia,
-} from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
-
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [arbitrum, arbitrumSepolia],
-  [publicProvider()]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: "RainbowKit App",
-  projectId: "YOUR_PROJECT_ID",
-  chains,
-});
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-  webSocketPublicClient,
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
